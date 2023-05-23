df29b5bd3520bd9182f52d620ea4d94cc1e95d53
swforrest <scottwforrest@gmail.com>
2023-05-23 15:54:44 +1000

    Added separate script for importing the habitat layers as it is similar across modelling scripts

---
+++ SSA habitat layers.R
@@ -0,0 +1,42 @@
+### Importing habitat layers
+
+ndvi_projected <- rast("mapping/cropped rasters/ndvi_GEE_projected_watermask20230207.tif")
+
+# scaling from the buffalo data
+scaling_mean <- as.numeric(attributes(buffalo_CLR_year$ndvi_scaled)[1]) # 0.3125089
+scaling_sd <- as.numeric(attributes(buffalo_CLR_year$ndvi_scaled)[2]) # 0.143179 for July 2018
+ndvi_stack_scaled <- (ndvi_projected - scaling_mean) / scaling_sd
+# plot(ndvi_stack_scaled)
+# time_vector <- terra::time(ndvi_projected)
+# terra::time(ndvi_stack_scaled) <- NULL
+# terra::time(ndvi_stack_scaled) <- lubridate::date(as.POSIXlt(as.POSIXlt("1970/01/01 00:00:00") + time_vector))
+# ndvi_above0_stack_scaled <- rast("mapping/cropped rasters/ndvi_above0_stack_scaled_20230210.tif")
+
+# elev <- rast("mapping/cropped rasters/DEM_H_raster.tif")
+slope <- rast("mapping/cropped rasters/slope_raster.tif")
+veg_herby <- rast("mapping/cropped rasters/veg_herby.tif")
+canopy_cover <- rast("mapping/cropped rasters/canopy_cover.tif")
+
+# names(slope) <- "Slope"
+# names(veg_herby) <- "Herbaceous vegetation"
+# names(canopy_cover) <- "Canopy cover"
+
+# plot(elev)
+# plot(slope)
+# plot(veg_herby)
+# plot(canopy_cover)
+# plot(ndvi_july2018)
+
+# hist(buffalo_CLR_year$slope_scaled)
+# slope_scaled <- (slope - attr(buffalo_CLR_year$slope_scaled, "scaled:center")) / attr(buffalo_CLR_year$slope_scaled, "scaled:scale")
+# plot(slope_scaled)
+# 
+# writeRaster(slope_scaled, "mapping/cropped rasters/slope_scaled_by_buffalo_data_20230210.tif")
+slope_scaled <- rast("mapping/cropped rasters/slope_scaled_by_buffalo_data_20230210.tif")
+
+# hist(buffalo_CLR_year$canopy_scaled)
+# canopy_scaled <- ((canopy_cover / 100) - attr(buffalo_CLR_year$canopy_scaled, "scaled:center")) / attr(buffalo_CLR_year$canopy_scaled, "scaled:scale")
+# plot(canopy_scaled)
+# 
+# writeRaster(canopy_scaled, "mapping/cropped rasters/canopy_scaled_by_buffalo_data_20230210.tif")
+canopy_scaled <- rast("mapping/cropped rasters/canopy_scaled_by_buffalo_data_20230210.tif")
